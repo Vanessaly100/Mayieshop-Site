919c0566809ad3bafb3b38c799b6543c2a2fba15
Vanessaly100 <vanessalydia360@gmail.com>
2024-07-02 01:53:47 +0100

    added section four, five and six also added icons folder and tiny slider

---
+++ slider.js
@@ -0,0 +1,30 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 4,
+    slideBy: 1,
+    autoplay: true,
+    controls: false,
+   controlsPosition: 'false',
+  navPosition: 'bottom',
+  mouseDrag: true,
+    autoplayButtonOutput: false,
+    nav: true,
+
+responsive: {
+    0: {
+        items: 1 // 1 item for mobile devices
+    },
+    640: {
+        items: 2 // 2 items for tablets
+    },
+    940: {
+        items: 3 // 2 items for tablets
+    },
+    // 1223: {
+    //     items: 3 // 2 items for tablets
+    // },
+    1024: {
+        items: 4 // 4 items for desktops
+    }
+}
+  });
